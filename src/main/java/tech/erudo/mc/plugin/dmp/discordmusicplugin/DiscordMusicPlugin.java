66f0195ef9918dc5fbe179e9188c55a9a41a3b21
erudosan0524-JP <hashierudo@gmail.com>
2022-04-09 13:52:27 +0900

    first commit

---
+++ src/main/java/tech/erudo/mc/plugin/dmp/discordmusicplugin/DiscordMusicPlugin.java
@@ -0,0 +1,16 @@
+package tech.erudo.mc.plugin.dmp.discordmusicplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DiscordMusicPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
